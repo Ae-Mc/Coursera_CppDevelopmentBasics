1d4cc8cbe2298bb5274cabed40d2ad3735b218aa
Ae-Mc <ae_mc@mail.ru>
2021-05-20 01:27:27 +0300

    Задание «Перебор перестановок»

---
+++ Week4/Permutations/Permutations.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+vector<vector<T>> GetPermutations(const vector<T>& basePermutation) {
+    vector<vector<T>> result{basePermutation};
+    vector<T> currentPermutation = basePermutation;
+    while(prev_permutation(currentPermutation.begin(), currentPermutation.end())) {
+        result.push_back(currentPermutation);
+    }
+    return result;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> v(N);
+    for(int i = N; i > 0; i--) {
+        v[N - i] = i;
+    }
+    auto permutations = GetPermutations(v);
+    for(const auto& permutation : permutations) {
+        for(const auto& num : permutation) {
+            cout << num << ' ';
+        }
+        cout << endl;
+    }
+}
